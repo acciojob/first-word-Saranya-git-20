4d92f0b21111cb8f7cad26fe1a220d0e61449df5
Saranya Barani <62207142+Saranya-git-20@users.noreply.github.com>
2025-08-31 22:11:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function firstWord(s) {
   // your code here
+	words = s.split(" ")
+	return words[0]
 }
 
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
